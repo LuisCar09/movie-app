59961cb76f2119be08fa5861c4157df32ee230ed
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-08-10 14:02:21 +0200

    improvements in js

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ const API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc
 
 
 const fetchTheMovies = async () => {
-    const getMovieData = await fetch (`${API}`)
+    const getMovieData = await fetch (API)
     const data = await getMovieData.json()
+    const movies = data.results
 
+    movies.forEach(movie => {
     const specificInfo = {
-        image: results.poster_path,
-        title: results.title,
-        overview: results.overview,
-        rating: results.vote_average
+        image: movies.poster_path,
+        title: movies.title,
+        overview: movies.overview,
+        rating: movies.vote_average
+    }
+    addMovies(specificInfo)
+})
+
+const addMovies = (moviesInfo) => {
+    const {image,title,overview,rating} = moviesInfo;
+
+    const template = `
+    <div class="movie">
+        <div class="image"><img src="${image}" alt="poster"></div>
+        <div class="title"><h2>${title}</h2></div>
+        <div class="overview"><p>${overview}</h2></div>
+        <div class="rating"><p>${rating}</p></div>
+    </div>
+    `
+    allMovies.innerHTML += template;
     }
 }
+
+fetchTheMovies();
